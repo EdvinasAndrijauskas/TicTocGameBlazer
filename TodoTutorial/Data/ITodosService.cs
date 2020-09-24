4f972ab0473e079ae3e1c3f71a45662f4c2c40a6
unknown <edvinas.andrijauskas2000@gmail.com>
2020-09-24 17:55:10 +0200

    First commit Nothing much

---
+++ TodoTutorial/Data/ITodosService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TodoTutorial.Models;
+
+namespace TodoTutorial.Data
+{
+    public interface ITodosService
+    {
+        IList<Todo> GetTodos();
+        void AddTodo(Todo todo);
+        public void RemoveTodo(int todoId);
+    }
+}
